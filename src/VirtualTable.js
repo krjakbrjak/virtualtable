31125788238a5ba5fd6f4872aaf79ea0e2d81e41
Nikita Vakula <programmistov.programmist@gmail.com>
2023-11-08 21:25:17 +0100

    Reworked VirtualTable component
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- src/VirtualTable.js
+++ src/VirtualTable.js
@@ -4,14 +4,39 @@
  * @author Nikita Vakula <programmistov.programmist@gmail.com>
  */
 
-import React, { useEffect, useState, useRef } from 'react';
+import React, {
+    useReducer, useEffect, useState, useRef,
+} from 'react';
 import PropTypes from 'prop-types';
 
-import { usePage } from './hooks';
 import { slideItems } from './helpers/collections';
 
 import './base.css';
 import css from './VirtualTable.css';
+
+import { LazyPaginatedCollection } from './helpers/LazyPaginatedCollection';
+
+/**
+ * Reducer function for managing state changes.
+ *
+ * @param {Object} state - The current state of the application.
+ * @param {Object} action - The action object that describes the state change.
+ * @param {string} action.type - The type of the action.
+ * @param {any} [action.data] - Additional data associated with the action.
+ * @returns {Object} - The new state after applying the action.
+ */
+const reducer = (state, action) => {
+    switch (action.type) {
+    case 'scroll':
+        return { ...state, scrollTop: action.data };
+    case 'render':
+        return { ...state, itemHeight: action.data };
+    case 'loaded':
+        return { ...state, ...action.data };
+    default:
+        return state;
+    }
+};
 
 /**
  * @callback VirtualTable.render
@@ -38,42 +63,95 @@ import css from './VirtualTable.css';
  */
 const VirtualTable = ({ height, renderer, fetcher }) => {
     const ref = useRef(null);
+    const [collection, setCollection] = useState(new LazyPaginatedCollection(1, fetcher));
 
-    const {
-        page,
-        setPageCount,
-        pageOffset,
-        setPageOffset,
-    } = usePage(fetcher);
+    const [state, dispatch] = useReducer(reducer, {
+        scrollTop: 0,
+        itemHeight: 0,
+        itemCount: 0,
+        items: [],
+        offset: 0,
+    });
 
-    const totalHeight = () => {
-        if (ref.current && ref.current.children.length) {
-            return ref.current.children[0].clientHeight * page.totalCount;
+    const [currentOffset, setCurrentOffset] = useState(0);
+
+    const calculatePageCount = () => 2 * Math.floor(height / state.itemHeight);
+
+    useEffect(() => {
+        collection.slice(0, 1).then((result) => {
+            dispatch({
+                type: 'loaded',
+                data: {
+                    ...result,
+                    itemCount: collection.count(),
+                },
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        if (state.itemHeight !== 0) {
+            const offset = Math.floor(state.scrollTop / state.itemHeight);
+            collection.slice(offset, collection.pageSize()).then((result) => {
+                dispatch({
+                    type: 'loaded',
+                    data: {
+                        ...result,
+                        itemCount: collection.count(),
+                    },
+                });
+            });
         }
+    }, [collection]);
 
-        return 0;
-    };
+    useEffect(() => {
+        if (state.itemHeight) {
+            const offset = Math.floor(state.scrollTop / state.itemHeight);
+            setCurrentOffset(offset);
+            const c = calculatePageCount();
+            if (c !== collection.pageSize()) {
+                setCollection(new LazyPaginatedCollection(c, fetcher));
+            } else {
+                collection.slice(offset, collection.pageSize()).then((result) => {
+                    if (currentOffset !== result.offset) {
+                        dispatch({
+                            type: 'loaded',
+                            data: {
+                                ...result,
+                                itemCount: collection.count(),
+                            },
+                        });
+                    }
+                });
+            }
+        }
+    }, [
+        state,
+    ]);
 
-    const calculatePageCount = () => 2 * Math.floor(height / ref.current.children[0].clientHeight);
-
-    const generate = (d) => {
+    const generate = (offset, d) => {
         const ret = [];
-        for (let i = 0; i < page.items.length; i += 1) {
-            ret.push(<div key={i + pageOffset}>{renderer(d[i])}</div>);
+        for (let i = 0; i < d.length; i += 1) {
+            ret.push(<div key={i + offset}>{renderer(d[i])}</div>);
         }
         return ret;
     };
 
     useEffect(() => {
-        if (ref.current && ref.current.children) {
-            const c = calculatePageCount();
-            if (c !== page.items.length) {
-                setPageCount(c);
+        if (ref.current) {
+            ref.current.scrollTop = state.scrollTop % state.itemHeight;
+            if (ref.current.children && ref.current.children.length) {
+                if (ref.current.children[0].clientHeight !== state.itemHeight) {
+                    dispatch({
+                        type: 'render',
+                        data: ref.current.children[0].clientHeight,
+                    });
+                }
             }
         }
     });
 
-    if (page.items.length === 0) {
+    if (state.items.length === 0) {
         return null;
     }
 
@@ -86,24 +164,24 @@ const VirtualTable = ({ height, renderer, fetcher }) => {
                     height, position: 'sticky', top: 0, width: '100%', overflow: 'hidden',
                 }}
             >
-                {generate(slideItems(pageOffset, page))}
+                {generate(currentOffset, slideItems(currentOffset, {
+                    items: state.items,
+                    offset: state.offset,
+                }))}
             </div>
             <div
                 style={{
                     height, overflow: 'scroll', position: 'absolute', width: '100%', top: 0,
                 }}
                 onScroll={(e) => {
-                    ref.current.scrollTop = e.target.scrollTop
-                        % ref.current.children[0].clientHeight;
-                    const tmp = Math.floor(e.target.scrollTop
-                            / ref.current.children[0].clientHeight);
-                    if (tmp !== pageOffset) {
-                        setPageOffset(tmp);
-                    }
+                    dispatch({
+                        type: 'scroll',
+                        data: e.target.scrollTop,
+                    });
                 }}
             >
                 <div style={{
-                    height: `${totalHeight()}px`, position: 'absolute', top: 0, width: '100%',
+                    height: `${state.itemCount * state.itemHeight}px`, position: 'absolute', top: 0, width: '100%',
                 }}
                 />
             </div>
